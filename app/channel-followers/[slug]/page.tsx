1e607f0a0886dc3bef3785402d282021cdae479d
artlu99 <alu99.sm@gmail.com>
2024-03-09 18:57:22 -0500

    channel-followers route

---
+++ app/channel-followers/[slug]/page.tsx
@@ -0,0 +1,59 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+type UsersResponse = {
+    users: {
+        fid: number,
+        username?: string,
+        verifications?: string[]
+    }[],
+    next: {cursor: string}
+}
+
+const api_key = process.env.NEXT_PUBLIC_NEENOO_API_KEY || "no API key";
+
+export default function FollowersCSV({ params }: { params: { slug: string } }) {
+  const [results, setResults] = useState<UsersResponse|undefined>(undefined);
+
+  const slug = params.slug;
+  const [channelId] = slug.split(".");
+
+  useEffect(() => {
+    async function fetchAsync() {
+      const newResults = await fetch(
+        `https://api.neynar.com/v2/farcaster/channel/followers?id=${channelId}&limit=100`,
+        {
+          method: "GET",
+          headers: {
+            accept: "application/json",
+            api_key: api_key,
+          },
+        }
+      );
+      try {
+        const newResultsJson = await newResults.json();
+        setResults(newResultsJson as UsersResponse);
+      } catch (err) {
+        console.error(err);
+      }
+    }
+
+    fetchAsync();
+  }, [channelId]);
+
+  const csv = results
+    ? results.users.map((r, i) => {
+        const { fid, username, verifications } = r;
+        return verifications && verifications.length > 0
+          ? `${i},${channelId},${fid},${username},${verifications[0]}\n`
+          : undefined;
+      })
+    : "there was a problem, results not available";
+
+  return (
+    <div>
+      <pre>{csv}</pre>
+    </div>
+  );
+}
---
+++ app/channel-followers/page.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+export default function ChannelFollowers() {
+  const pathname = usePathname();
+
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-0">
+      <div className="z-10 w-full flex flex-col font-mono text-sm lg:flex">
+        For illustration only, connected addresses for the first 100 channel followers are shown here as a CSV
+        <br />
+        <i>e.g., </i>
+        <Link href={`${pathname}/higher.csv`}>{pathname}/higher.csv</Link>
+        <br />
+        <Link href="/" >see Main Page for link to Github (MIT-licensed)</Link>
+      </div>
+    </main>
+  );
+}
